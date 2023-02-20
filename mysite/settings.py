a686b6bc37012f3850e4cda84d135e4be82611bd
Kesseki <kesseki@Kessekis-MBP.attlocal.net>
2023-02-20 06:20:59 -0500

    after EB

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,13 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SK')
-SECRET_KEY = 'django-insecure-*&l0nse3v)0njh6rvsb3_g$!lq6e^1np(&y8xfm8%1+%l_d^9^'
+SECRET_KEY = os.environ.get('SK')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'PollsApp37-dev.us-east-1.elasticbeanstalk.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
