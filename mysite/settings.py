aaa9ac9b133e4902a1bbc9ba68e2b477cca8ab93
Kesseki <zl3952@nyu.edu>
2023-03-02 17:08:02 -0500

    updates for eb

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SK')
+SECRET_KEY = 'django-insecure-*&l0nse3v)0njh6rvsb3_g$!lq6e^1np(&y8xfm8%1+%l_d^9^'
+#os.environ.get('SK')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
